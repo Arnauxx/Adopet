9073e8eb797dff73590b05014dac4251d4580b48
Arnaux <arnauxx@hotmail.com>
2025-03-12 23:03:02 -0300

    Aplicando teste de unidade no comando import

--- Alura.Adopet.Testes/ImportTest.cs
+++ Alura.Adopet.Testes/ImportTest.cs
@@ -14,27 +14,24 @@ namespace Alura.Adopet.Testes
     public class ImportTest
     {
         [Fact]
-        public async void QuandoAPIEstaNoArDeveRetornarListaDePet()
+        public async void QuandoListaVaziaNaoDeveChamarCreatePetAsync()
         {
             //Arrange
             var leitorDeArquivo = new Mock<LeitorDeArquivo>(MockBehavior.Default, It.IsAny<string>());
             var listaDePet = new List<Pet>();
 
-            var pet = new Pet(new Guid("456b24f4-19e2-4423-845d-4a80e8854a41"),
-                 "Lima", TipoPet.Cachorro); //"456b24f4-19e2-4423-845d-4a80e8854a41; Lima Limão;1";
-            listaDePet.Add(pet);
-
             leitorDeArquivo.Setup(_ => _.RealizaLeituraDoArquivo()).Returns(listaDePet);
 
-            var httpClientPet = new HttpClientPet(new AdopetAPIClientFactory().CreateClient("adopet"));
-            var import = new Import(httpClientPet, leitorDeArquivo.Object);
+            var httpClientPet = new Mock<HttpClientPet>(MockBehavior.Default, It.IsAny<HttpClient>());
+
+            var import = new Import(httpClientPet.Object, leitorDeArquivo.Object);
             string[] args = { "import", "lista.csv" };
+
             //Act
             await import.ExecutarAsync(args);
-            var listaPet = await httpClientPet.ListPetsAsync();
-            //Assert
-            Assert.NotNull(listaPet);
 
+            //Assert
+            httpClientPet.Verify(_=>_.CreatePetAsync(It.IsAny<Pet>()), Times.Never());
         }
     }
 }
