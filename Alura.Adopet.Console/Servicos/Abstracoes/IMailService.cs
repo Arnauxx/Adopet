356f7cf6009f47246cf04070a29b33b2f75d9853
Arnaux <arnauxx@hotmail.com>
2025-04-02 20:45:09 -0300

    Implantando o envio de email

---
+++ Alura.Adopet.Console/Servicos/Abstracoes/IMailService.cs
@@ -0,0 +1,10 @@
+﻿namespace Alura.Adopet.Console.Servicos.Abstracoes
+{
+    public interface IMailService
+    {
+        Task SendmailAssync(string rementente,
+            string destinatario,
+            string titulo,
+            string corpo);
+    }
+}
