88509df72b527053fbf18c89d2d20f9e6f990e02
Arnaux <arnauxx@hotmail.com>
2025-04-03 08:32:58 -0300

    Capturando evento de importacao

--- Alura.Adopet.Console/Comandos/Import.cs
+++ Alura.Adopet.Console/Comandos/Import.cs
@@ -8,7 +8,7 @@ namespace Alura.Adopet.Console.Comandos
 {
     [DocComando(instrucao: "import",
         documentacao: "adopet import <ARQUIVO> comando que realiza a importação do arquivo de pets.")]
-    public class Import : IComando
+    public class Import : IComando,IDepoisDaExecucao
     {
 
         private readonly IApiService<Pet> clientPet;
@@ -19,6 +19,8 @@ namespace Alura.Adopet.Console.Comandos
             this.clientPet = clientPet;
             this.leitorDeArquivo = leitorDeArquivo;
         }
+
+        public event Action<Result>? DepoisDaExecucao;
 
         public async Task<Result> ExecutarAsync()
         {
@@ -34,7 +36,9 @@ namespace Alura.Adopet.Console.Comandos
                 {
                     await clientPet.CreateAsync(pet);
                 }
-                return Result.Ok().WithSuccess(new SuccessWithPets(listaDePet, "Importação realizada com sucesso!"));
+                var resultado = Result.Ok().WithSuccess(new SuccessWithPets(listaDePet, "Importação realizada com sucesso!"));
+                DepoisDaExecucao?.Invoke(resultado);
+                return resultado;
             }
             catch (Exception ex)
             {
