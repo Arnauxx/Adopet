201e4816b1e97b0806307260d55e82326deb4826
Arnaux <arnauxx@hotmail.com>
2025-04-02 21:17:38 -0300

    User secrets e preparacao do ambiente para o serviço de envio de email

--- Alura.Adopet.Console/Settings/Configurations.cs
+++ Alura.Adopet.Console/Settings/Configurations.cs
@@ -9,18 +9,31 @@ namespace Alura.Adopet.Console.Settings
             return new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets("c99257e1-aeab-4798-8c8e-113f9cef3893")
                 .Build();
         }
 
-        public static AppSettings ApiSetting
+        public static ApiSettings ApiSetting
         {
             get
             {
                 var _config = BuildConfiguration();
                 return _config
-                    .GetSection(AppSettings.Section)
-                    .Get<AppSettings>() ??
+                    .GetSection(ApiSettings.Section)
+                    .Get<ApiSettings>() ??
                     throw new ArgumentException("Seção para configuração da API não encontrada!");
+            }
+        }
+
+        public static MailSettings MailSetting
+        {
+            get
+            {
+                var _config = BuildConfiguration();
+                return _config
+                    .GetSection(MailSettings.EmailSection)
+                    .Get<MailSettings>() ??
+                    throw new ArgumentException("Seção para configuração do e-mail não encontrada!");
             }
         }
     }
